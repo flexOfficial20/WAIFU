238ef95ba2b07c60982cc67c7e4c0d4f5ff3c029
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-09-04 12:59:05 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -3,15 +3,15 @@ class Config(object):
 
     # Get this value from my.telegram.org/apps
     OWNER_ID = "7453770651"
-    sudo_users = "7453770651", "6996612518", "6454320047", "6257270528", "6574393060", "1292648081", "6532639723", "6453545159", "6806075764", "5610811504"
+    sudo_users = "7453770651", "7499006737", "7096345827"
     GROUP_ID = -1001975521991
-    TOKEN = "6993647727:AAGEsZVoOEPpKuuGwrnHc1o1bQoCLRAykAI"
+    TOKEN = "7378765314:AAGrT1K6wL98JBlaEXbCOJ0Bm9O5ZyqzHWQ"
     mongo_url = "mongodb+srv://vikas:vikas@vikas.yfezexk.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/dc0aa314d28a67af0ee83.jpg", "https://telegra.ph/file/e3bdc6e1f14191e058ea7.jpg", "https://telegra.ph/file/dc0aa314d28a67af0ee83.jpg"]
-    SUPPORT_CHAT = "-1001975521991"
-    UPDATE_CHAT = "Nᴀʀᴜᴛᴏ Uᴘᴅᴀᴛᴇs"
-    BOT_USERNAME = "nudeXcatchrbot"
-    CHARA_CHANNEL_ID = "-1001975521991"
+    SUPPORT_CHAT = "-1002154364668"
+    UPDATE_CHAT = "LUFFY BOTS"
+    BOT_USERNAME = "capture_Waifu_Bot"
+    CHARA_CHANNEL_ID = "-1002154364668"
     api_id = 22792918
     api_hash = "ff10095d2bb96d43d6eb7a7d9fc85f81"
     
