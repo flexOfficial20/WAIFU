3354898ba3e1da5917135522faa62f151be90c1b
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-09-06 22:12:30 +0530

    Update upload.py

--- shivu/modules/upload.py
+++ shivu/modules/upload.py
@@ -14,7 +14,7 @@ img_url character-name anime-name rarity-number
 
 use rarity number accordingly rarity Map
 
-rarity_map = 1 (⚪️ Common), 2 (🟣 Rare) , 3 (🟡 Legendary), 4 (🟢 Medium)"""
+rarity_map = (⚪ Common=1) (🟢 Medium=2) (🟠 Rare=3) (🟡 Legendary=4) (💠 Cosmic=5) (💮 Exclusive=6) (🔮 Limited Edition=7)"""
 
 
 # Function to handle character ID sequence
@@ -79,7 +79,7 @@ async def upload(update: Update, context: CallbackContext) -> None:
             await update.message.reply_text('Failed to upload image to ImgBB.')
             return
 
-        rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium"}
+        rarity_map = {1: "⚪ Common", 2: "🟠 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💠 Cosmic", 6: "💮 Exclusive", 7: "🔮 Limited Edition"}
         try:
             rarity = rarity_map[int(args[3])]
         except KeyError:
@@ -165,7 +165,7 @@ async def update(update: Update, context: CallbackContext) -> None:
         if args[1] in ['name', 'anime']:
             new_value = args[2].replace('-', ' ').title()
         elif args[1] == 'rarity':
-            rarity_map = {1: "⚪ Common", 2: "🟣 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💮 Special edition"}
+            rarity_map = {1: "⚪ Common", 2: "🟠 Rare", 3: "🟡 Legendary", 4: "🟢 Medium", 5: "💠 Cosmic", 6: "💮 Exclusive", 7: "🔮 Limited Edition"}
             try:
                 new_value = rarity_map[int(args[2])]
             except KeyError:
