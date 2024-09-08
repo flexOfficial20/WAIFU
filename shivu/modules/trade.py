74814dfbb942787c9e05f2bd0c7617610fe2d41a
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-09-08 12:37:32 +0530

    Update trade.py

--- shivu/modules/trade.py
+++ shivu/modules/trade.py
@@ -3,6 +3,118 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from shivu import user_collection, shivuu
 
+pending_trades = {}
+
+
+@shivuu.on_message(filters.command("trade"))
+async def trade(client, message):
+    sender_id = message.from_user.id
+
+    if not message.reply_to_message:
+        await message.reply_text("You need to reply to a user's message to trade a character!")
+        return
+
+    receiver_id = message.reply_to_message.from_user.id
+
+    if sender_id == receiver_id:
+        await message.reply_text("You can't trade a character with yourself!")
+        return
+
+    if len(message.command) != 3:
+        await message.reply_text("You need to provide two character IDs!")
+        return
+
+    sender_character_id, receiver_character_id = message.command[1], message.command[2]
+
+    sender = await user_collection.find_one({'id': sender_id})
+    receiver = await user_collection.find_one({'id': receiver_id})
+
+    sender_character = next((character for character in sender['characters'] if character['id'] == sender_character_id), None)
+    receiver_character = next((character for character in receiver['characters'] if character['id'] == receiver_character_id), None)
+
+    if not sender_character:
+        await message.reply_text("You don't have the character you're trying to trade!")
+        return
+
+    if not receiver_character:
+        await message.reply_text("The other user doesn't have the character they're trying to trade!")
+        return
+
+
+
+
+
+
+    if len(message.command) != 3:
+        await message.reply_text("/trade [Your Character ID] [Other User Character ID]!")
+        return
+
+    sender_character_id, receiver_character_id = message.command[1], message.command[2]
+
+    
+    pending_trades[(sender_id, receiver_id)] = (sender_character_id, receiver_character_id)
+
+    
+    keyboard = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("Confirm Trade", callback_data="confirm_trade")],
+            [InlineKeyboardButton("Cancel Trade", callback_data="cancel_trade")]
+        ]
+    )
+
+    await message.reply_text(f"{message.reply_to_message.from_user.mention}, do you accept this trade?", reply_markup=keyboard)
+
+
+@shivuu.on_callback_query(filters.create(lambda _, __, query: query.data in ["confirm_trade", "cancel_trade"]))
+async def on_callback_query(client, callback_query):
+    receiver_id = callback_query.from_user.id
+
+    
+    for (sender_id, _receiver_id), (sender_character_id, receiver_character_id) in pending_trades.items():
+        if _receiver_id == receiver_id:
+            break
+    else:
+        await callback_query.answer("This is not for you!", show_alert=True)
+        return
+
+    if callback_query.data == "confirm_trade":
+        
+        sender = await user_collection.find_one({'id': sender_id})
+        receiver = await user_collection.find_one({'id': receiver_id})
+
+        sender_character = next((character for character in sender['characters'] if character['id'] == sender_character_id), None)
+        receiver_character = next((character for character in receiver['characters'] if character['id'] == receiver_character_id), None)
+
+        
+        
+        sender['characters'].remove(sender_character)
+        receiver['characters'].remove(receiver_character)
+
+        
+        await user_collection.update_one({'id': sender_id}, {'$set': {'characters': sender['characters']}})
+        await user_collection.update_one({'id': receiver_id}, {'$set': {'characters': receiver['characters']}})
+
+        
+        sender['characters'].append(receiver_character)
+        receiver['characters'].append(sender_character)
+
+        
+        await user_collection.update_one({'id': sender_id}, {'$set': {'characters': sender['characters']}})
+        await user_collection.update_one({'id': receiver_id}, {'$set': {'characters': receiver['characters']}})
+
+        
+        del pending_trades[(sender_id, receiver_id)]
+
+        await callback_query.message.edit_text(f"You have successfully traded your character with {callback_query.message.reply_to_message.from_user.mention}!")
+
+    elif callback_query.data == "cancel_trade":
+        
+        del pending_trades[(sender_id, receiver_id)]
+
+        await callback_query.message.edit_text("❌️ Sad Cancelled....")
+
+
+
 
 pending_gifts = {}
 
