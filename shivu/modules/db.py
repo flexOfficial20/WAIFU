86a7bd3c98589bc0eeb66914caf6faf37d1dde46
vikas-851 <167636013+vikas-851@users.noreply.github.com>
2024-09-13 20:04:50 +0530

    Update db.py

--- shivu/modules/db.py
+++ shivu/modules/db.py
@@ -1,4 +1,4 @@
-from telegram import Update, InputFile
+from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, filters, CallbackContext
 from shivu import collection, application
 import io
@@ -24,4 +24,4 @@ async def handle_photo(update: Update, context: CallbackContext) -> None:
 
 # Add handlers for the /name command and photo messages
 application.add_handler(CommandHandler("name", handle_photo))
-application.add_handler(MessageHandler(filters._photo, handle_photo))
+application.add_handler(MessageHandler(filters.photo, handle_photo))
